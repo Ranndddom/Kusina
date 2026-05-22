793bbcc4d21a6c7066f9f013184bba329bc4b1a9
Ranndddom <zegnamolinar77@gmail.com>
2026-05-22 14:03:06 +0800

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import myLogo from '/src/assets/Kusina.png';
 import { 
   PlusCircle, ShoppingCart, Database, Sparkles, 
   Plus, X, Edit3, Trash2, Search, CheckCircle, AlertTriangle, ChevronRight, RefreshCw
@@ -537,7 +538,7 @@ export default function App() {
         <div className="container mx-auto max-w-5xl flex justify-between items-center h-[36px]">
           <div className="flex items-center cursor-pointer h-full" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
             <div className="mr-2.5 flex items-center justify-center w-[35px] h-[35px] overflow-hidden rounded-md bg-[#f0f0f0]">
-              <img src="./public/Kusina.png" alt="Logo" className="w-full h-full object-cover" />
+              <img src="src/assets/Kusina.png" alt="Logo" className="w-full h-full object-cover" />
             </div>
             <div className="flex items-center">
               <h5 className="font-bold mb-0 text-[#3e3835] tracking-tight text-[20px] flex items-center">Kusina</h5>
